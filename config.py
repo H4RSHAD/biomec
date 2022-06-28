35fc8a44f0e9a9d4a73b493ee5a9f2aeb3defd5d
cristian <cristhianarauz06@gmail.com>
2022-06-28 11:58:36 -0400

    1234

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv  # Instalar con pip install python-dotenv
 load_dotenv()                   # Cargar todo el cotenido de .env en variables de entorno
 
 class Config:
-
+    #cristian estuvo aquí
     # Trabajar en la nube voladora. Estamos usando Heroku, es una plataforma como servicio en la nube 
     SERVER_NAME = "biomec.herokuapp.com" # Esto es el nombre del servidor, para mostrar ONLINE 
     DEBUG = True
