be4fab33f4502f5bb46b03a8e473f7b332b6848f
cristian <cristhianarauz06@gmail.com>
2022-06-28 15:08:48 -0400

    1234

--- biomec/database/DB_Lab.sql
+++ biomec/database/DB_Lab.sql
@@ -241,11 +241,11 @@ insert into Persona values(10,'Veronica', 'Carrizales', 'Tasima',609056,'Carriza
 insert into Persona values(11,'Katherin', 'Arteaga', 'Melgar',null,'Arteaga44@hemovirtual.com','1990-10-07');
 
 --ADMIN
-insert into Persona values(12,'CRISTIAN','CUELLAR','SERRANO',78021105,'ccuellar260@gmail.com','1990-10-07');
-insert into Persona values(13,'LUIS EDUARDO','CESPEDES','VARGAS',77679046,'llxxlaloxxll@gmail.com','1990-10-07');
-insert into Persona values(14,'BEBI','VARGAS','RIOS',70033034,'bbithalindaa@gmail.com','1990-10-07');
-insert into Persona values(15,'NELSON','PANIAGUA','PORRAS',79492778,'nelson95sc@gmail.com','1990-10-07');
-insert into Persona values(16,'FRANZ','RIBERA','SAAVEDRA',78596734,'ribera.franz@ficct.uagrm.edu.bo','1990-10-07');
+insert into Persona values(12,'CRISTIAN','CUELLAR','SERRANO',78021105,'ccuellar260@gmail.com','1990-10-07')
+insert into Persona values(13,'LUIS EDUARDO','CESPEDES','VARGAS',77679046,'llxxlaloxxll@gmail.com','1990-10-07')
+insert into Persona values(14,'BEBI','VARGAS','RIOS',70033034,'bbithalindaa@gmail.com','1990-10-07')
+insert into Persona values(15,'NELSON','PANIAGUA','PORRAS',79492778,'nelson95sc@gmail.com','1990-10-07')
+insert into Persona values(16,'FRANZ','RIBERA','SAAVEDRA',78596734,'ribera.franz@ficct.uagrm.edu.bo','1990-10-07')
 
 
 
@@ -275,18 +275,18 @@ insert into Privilegio_Rol values(5,3);
 insert into Privilegio_Rol values(5,4);
 
 -- usuarios administradores
-insert into Usuario values(1,'cristian','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',1,12);
-insert into Usuario values(2,'luis','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',1,13);
-insert into Usuario values(3,'bebi','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',1,14);
-insert into Usuario values(4,'nelson','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',1,15);
-insert into Usuario values(5,'franz','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',1,16);
+insert into Usuario values(1,'cristian','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',1,12)
+insert into Usuario values(2,'luis','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',1,13)
+insert into Usuario values(3,'bebi','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',1,14)
+insert into Usuario values(4,'nelson','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',1,15)
+insert into Usuario values(5,'franz','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',1,16)
 
 -- usuarios de personal ( recepcionista)
-insert into Usuario values(6,'sandra','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',2,1);
+insert into Usuario values(6,'sandra','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',2,1)
 
 
 -- usuarios de Tecnico o el doctor ( Laboratorista)
-insert into Usuario values(7,'fatima','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',3,5);
+insert into Usuario values(7,'fatima','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',3,5)
 
 -- usuarios de Paciente 
 insert into Usuario values(8,'Yesmin','pbkdf2:sha256:260000$UJdTDI1U1yespnS4$5ec3974d5906c6e3e28a87da7e44e191eeecba9a794ff4bf368072eac040d11e',4,7);
@@ -474,32 +474,3 @@ insert into Recibo_Analisis values (4040,'2022-01-15',80,3,10);
 insert into Detalle values (2020,1);
 insert into Detalle values (3030,2);
 insert into Detalle values (4040,5);
-
-
-
-
-
-SELECT L_Analisis.id, L_Analisis.nombre, L_Analisis.precio 
-FROM L_Analisis
-ORDER BY ID DESC
-
-SELECT inventario.id, inventario.nombre, inventario.cantidad, persona.nombre 
-FROM Inventario,Laboratorista,Persona 
-where id_lb = id_laboratorista  and id_laboratorista =ci 
-ORDER BY ID DESC
-
-SELECT laboratorista.id_laboratorista, persona.nombre, persona.apellidop, persona.apellidom, persona.telefono, persona.fecha_nacimiento, especialida_med.nombre_esp
-FROM Laboratorista,Persona, Especialida_Med
-where id = id_esp_med  and id_laboratorista =ci 
-ORDER BY ID_Laboratorista DESC
-
-
-SELECT personal.id_persona, persona.nombre, persona.apellidop, persona.apellidom, persona.telefono, persona.fecha_nacimiento, cargo.nombre
-FROM Personal,Persona, Cargo
-where id_cargo = id_crg  and id_persona =ci 
-ORDER BY ID_Persona DESC
-
-SELECT paciente.id_paciente, persona.nombre, persona.apellidop, persona.apellidom, persona.telefono, persona.fecha_nacimiento, seguro.nombre_seguro
-FROM Paciente,Persona,Seguro 
-where id_paciente =ci and nro_seguro = nro_sg
-ORDER BY ID_Paciente DESC
